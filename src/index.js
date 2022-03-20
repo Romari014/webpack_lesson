caec6c1c3572478ebdfcd13f4dbfa699df925cb8
Roman <roma00062@gmail.com>
2022-03-20 20:29:12 +0100

    Installed: style-loader, css-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {config} from './modules/config'
 import AppService from './modules/app.service'
 import './modules/header.component'
+import './css/index.css'
+
 
 console.log("Config key: ", config.key);
 
