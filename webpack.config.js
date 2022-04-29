83de7809b8ad7c46128f2a3dc6dcbf0651d3600e
Roman <roma00062@gmail.com>
2022-04-29 20:37:06 +0200

    Added webpack plugin MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,17 +10,26 @@ module.exports = {
         filename:'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin({}),
+            new UglifyJsPlugin()
+        ]
+    },
     plugins: [
         new HTMLPlugin({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
         })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use:  ["style-loader", "css-loader"]
+                use:  [MiniCssExtractPlugin.loader, "css-loader"]
             }
         ]
     }
